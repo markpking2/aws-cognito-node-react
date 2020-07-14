4d7fccae3e911dadbcb3091bbe7c4cb13e8832c1
Mark King <markpkng@gmail.com>
2020-07-13 23:41:07 -0400

    auth functions

--- src/utils/cognitoAuth.js
+++ src/utils/cognitoAuth.js
@@ -14,5 +14,83 @@ Amplify.configure({
             redirectSignOut: cognitoConfig.signoutUri,
             responseType: "code",
         },
+        // storage: CustomChromeStorage
     },
 });
+
+async function signUp(email, password) {
+    return await Auth.signUp({
+        username: email,
+        password,
+    });
+}
+
+async function signIn(email, password) {
+    return await Auth.signIn(email, password);
+}
+
+async function confirmSignUp(email, code) {
+    return await Auth.confirmSignUp(email, code);
+}
+
+async function resendConfirmationCode(username) {
+    return await Auth.resendSignUp(username);
+}
+
+// pass in true to sign out from all devices
+async function signOut(global = false) {
+    return await Auth.signOut({ global });
+}
+
+async function federatedSignIn(provider) {
+    return await Auth.federatedSignIn({ provider });
+}
+
+async function forgotPassword(email) {
+    return await Auth.forgotPassword(email);
+}
+
+async function forgotPasswordSubmit(email, code, newPassword) {
+    try {
+        await Auth.forgotPasswordSubmit(email, code, newPassword);
+        return "Password was changed successfully.";
+    } catch (err) {
+        throw err;
+    }
+}
+
+async function changePassword(oldPassword, newPassword) {
+    try {
+        const user = await Auth.currentAuthenticatedUser();
+        await Auth.changePassword(user, oldPassword, newPassword);
+    } catch (err) {
+        throw err;
+    }
+}
+
+function getCurrentUser() {
+    return new Promise((resolve, reject) => {
+        Auth.currentSession()
+            .then((data) => {
+                const idToken = data.getIdToken();
+                const user = idToken.payload;
+                resolve(user);
+            })
+            .catch(() => {
+                reject(Error("Not signed in."));
+            });
+    });
+}
+
+export {
+    signUp,
+    signIn,
+    confirmSignUp,
+    resendConfirmationCode,
+    signOut,
+    federatedSignIn,
+    forgotPassword,
+    forgotPasswordSubmit,
+    changePassword,
+    getCurrentUser,
+};
