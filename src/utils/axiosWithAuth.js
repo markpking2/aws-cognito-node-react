6a7b156fd26ed2bcde8f0191b95d4ef3e78c34fa
Mark King <markpkng@gmail.com>
2020-07-14 02:48:48 -0400

    axiosWithAuth

---
+++ src/utils/axiosWithAuth.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { getIdToken } from "./amplifyAuth";
+
+export async function axiosWithAuth(method, path) {
+    try {
+        const idToken = await getIdToken();
+        return await axios
+            .create({
+                baseURL: "http://localhost:5000",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${idToken}`,
+                },
+            })
+            [method](path);
+    } catch (err) {
+        throw err;
+    }
+}
